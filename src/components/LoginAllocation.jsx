b4106804e9039a07470ef54d73be8d7ca0edf3e9
param-ninthu <ninthu1999@gmail.com>
2023-06-24 15:28:23 +0530

    login-aloocation layout

--- src/components/LoginAllocation.jsx
+++ src/components/LoginAllocation.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import "./../assets/css/login-allocation.css";
 
 const LoginAllocation = () => {
-  return <div>LoginAllocation</div>;
+  return (
+    <Container className="container" fluid>
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default LoginAllocation;
