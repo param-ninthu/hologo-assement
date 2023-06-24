f21197e5aeda96a4fb3b00fe47931a463b8d8efa
Arshaan <arshanarul@gmail.com>
2023-06-24 15:46:05 +0530

    arshaan-done

--- src/components/Loginpage.jsx
+++ src/components/Loginpage.jsx
@@ -1,9 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const Loginpage = () => {
+    const [textemail,setemail] = useState('');
+    const [textpassword , setpassword] = useState('');
+
+    const uploadstuff = async() => {
+       if(textemail=='email' && textpassword=='password') {
+        console.log('in');
+        // useNavigate('/');
+       }
+       else {
+        console.log('wrong-credentials');
+       }
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        await uploadstuff();
+    }
 return (
     <div>
-        
+              {[false].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">Hologo</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Hologo
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">usermanagement</Nav.Link>
+                  <Nav.Link href="#action2">emailallocation</Nav.Link>
+                 
+                </Nav>
+               
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+        <Form onSubmit={handleSubmit}>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="2">
+          Email
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="text" onChange={(e)=>setemail(e.target.value)} />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+        <Form.Label column sm="2">
+          Password
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="text" onChange={(e)=>setpassword(e.target.value)}  />
+        </Col>
+      </Form.Group>
+      <Button type='submit' variant="primary">Submit</Button>
+    </Form>
     </div>
 )
 }
